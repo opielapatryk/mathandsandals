5e9a51f94971350ad886af9a18ea3191f37cbd25
opielapatryk <xpatrykopiela@gmail.com>
2023-05-24 18:42:10 +0200

    almost done

--- Boss.cs
+++ Boss.cs
@@ -6,23 +6,21 @@ namespace swordsandsandals
         #region FIELDS
         public string Name;
         public int Health;
-        public int Agility;
         public int NextNum;
         public int NextNumSec;
         #endregion
         #region CONSTRUCTOR
-        public Boss(string name, int health, int agility, int nextNum, int nextNumSec)
+        public Enemy(string name, int health, int nextNum, int nextNumSec)
 		{
             Name = name;
             Health = health;
-            Agility = agility;
             NextNum = nextNum;
             NextNumSec = nextNumSec;
 
         }
         #endregion
         #region METHODS
-        public void BossAttack()
+        public void EnemyAttack()
         {
             //wypisz generate new math task
             //odliczanie(time)
